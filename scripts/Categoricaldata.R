5190a81d3ca18de256a08097afc454f8511fc0ae
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-03-26 11:01:49 +0100

    c

--- scripts/Categoricaldata.R
+++ scripts/Categoricaldata.R
@@ -9,16 +9,14 @@ library(ggplot2)
 library(Hmisc)
 
 
-#Setting working directory
-setwd("C:/Users/hnama/Desktop/MAK_TRAINING_IN R_2022/MAK_DAY6.26.3.22/Categorical Data")
-
-salaries<-read.csv("WorkSalaries.csv")
+salaries <- read.csv("data/WorkSalaries.csv")
 str(salaries)
 
-#categorical variables
-salaries$rank = as.factor(salaries$rank)
-salaries$discipline = as.factor(salaries$discipline)
-salaries$sex =as.factor(salaries$sex)
+# Categorical variables
+
+salaries$rank <- as.factor(salaries$rank)
+salaries$discipline <- as.factor(salaries$discipline)
+salaries$sex <- as.factor(salaries$sex)
 
 #----------------------------------------------------------
 
@@ -27,21 +25,22 @@ salaries$sex =as.factor(salaries$sex)
 # Recording an existing variable
 summary(salaries$salary)
 
-salaries$salarycat<-ifelse(salaries$salary < 113706, c("low"), c("high"))
+salaries$salarycat <- ifelse(salaries$salary < 113706, c("low"), c("high"))
 
-View(salaries)
+head(salaries)
 
-mytable<-table(salaries$rank, salaries$salarycat)
+mytable <- table(salaries$rank, salaries$salarycat)
+
 mytable
+
 addmargins(mytable, margin=c(1,2))
+
 prop.table(mytable)
+
 chisq.test(salaries$rank, salaries$salarycat)
 
 chisq.test(salaries$sex, salaries$salarycat )
 
-
 chisq.test(salaries$discipline, salaries$salarycat)
 
-
-
-#End
+# End
