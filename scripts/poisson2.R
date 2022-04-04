41d41cb7ce49178e14724e9adef9d25458a63dde
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-04-04 23:22:34 +0200

    c

--- scripts/poisson2.R
+++ scripts/poisson2.R
@@ -1,60 +1,38 @@
 #rm(list=ls())
 library(tidyverse)
-library(dplyr)
 library(readxl)
 library(readr)
 library(ggpubr)
-library(ggplot2)
 library(Hmisc)
-
-install.packages("faraway")
-library(faraway) #load the gala dataset
+library(faraway)
+library(qcc)
 data("gala")
 
-write.csv(gala, file = "gala.csv") # exporting data
+write.csv(gala, file = "data/gala.csv") # exporting data
 
-#set working directory
-setwd("C:/Users/hnama/Desktop/MAK_TRAINING_IN R_2022/MAK_DAY6.26.3.22/Poisson")
+gala <- read.csv("data/gala.csv")
 
-gala <- read.csv("gala.csv")
-
-print(head(gala))
-
-#or
-head(gala)
-
-
-#to get more insight about the data
-?gala
-
-#Species is a response variable.
 summary(gala)
-#We'll now study a basic summary of the variables.
-#Consider only predictor variables
+
+hist(gala$Species, breaks = 10, xlab = "Species Count", main = "Distribution of Species")
 
 
-# generate a histogram for Species in order to check if 
-#the variable follows the Poisson distribution
-
-hist(gala$Species, breaks = 10, xlab = "Species Count", main = "Distribution of Species", Prob = TRUE)
-#The above visualization shows that Species follows a Poisson distribution,
-#as the data is right-skewed. 
-
-#We can generate a boxplot too, to get more insight 
-#into the distribution pattern
 boxplot(gala$Species, main = "Boxplot of Species")
+
+boxplot(log(gala$Species), main = "Boxplot of natural log of Species")
 
 ###MORE TO MORE ADDITIONAL NOTES#######
 
-#Create a poisson model
+# Create a poisson model
 fit <- glm(Species ~ Endemics + Area + Elevation + Nearest + Scruz + Adjacent, 
          data = gala, family = poisson())
+
 summary(fit)
 
 #Based on the above analysis, we find that variables Endemics,
 #Area, and Nearest are significant and only
 #their inclusion is sufficient to build the 
-#right Poisson regression model. as there p-values<0.05
+#right Poisson regression model. as there p-values < 0.05
 
 #Next
 #We'll build a modified Poisson regression model taking 
@@ -64,11 +42,11 @@ summary(fit)
 fit.reduced <- glm(Species ~ Endemics + Area + Nearest, data = gala, family = poisson())
 summary(fit.reduced)
 
-#The output produces deviances, regression parameters,
-#and standard errors. We can see that each of the parameters
-#is significant at p < 0.05 level
+# The output produces deviances, regression parameters,
+# and standard errors. We can see that each of the parameters
+# is significant at p < 0.05 level
 
-##Interpreting coefficients#############
+## Interpreting coefficients#############
 coef(fit.reduced)
 
 #In Poisson regression, the dependent variable is modeled as
@@ -96,10 +74,7 @@ exp(coef(fit.reduced))
 #exponentiated parameters have a multiplicative rather
 #than an additive effect on the response variable.
 
-install.packages("qcc")
-library(qcc)
 qcc.overdispersion.test(gala$Species, type = "poisson")
-
 
 fit.od <- glm(Species ~ Endemics + Area + Nearest, data = gala, family = quasipoisson())
 summary(fit.od)
