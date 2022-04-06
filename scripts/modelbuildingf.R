ae61a1f7bbd9cc6d100b69c022799c9cd580d460
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-04-06 23:01:57 +0200

    c

--- scripts/modelbuildingf.R
+++ scripts/modelbuildingf.R
@@ -16,7 +16,7 @@ summary(statedata)
 g <- lm(Life.Exp ~., data = statedata)
 summary(g)
 
-# using olsrr with forward elimination
+# using olsrr with forward eliminati
 
 forward <- ols_step_forward_p(g, penter = 0.05)
 forward
